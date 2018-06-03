75c84211838cc859876c4d107ee8d2b560e02eb2
Odlopez <Odlopez@inbox.ru>
2018-06-03 07:34:13 +0300

    Группирует переменные и функции по объектам (как свойства и методы)

--- js/stat.js
+++ js/stat.js
@@ -1,55 +1,4 @@
 'use strict';
-var getСloudСoordinates = function (startingPointСoordinates, width, height, angle) {
-  var array = [];
-
-  var pointX = startingPointСoordinates[0] + angle;
-  var pointY = startingPointСoordinates[1];
-  array[0] = [pointX, pointY];
-
-  pointX = pointX + width - angle * 2;
-  array[1] = [pointX, pointY];
-
-  pointX = pointX + angle;
-  pointY = pointY + angle;
-  array[2] = [pointX, pointY];
-
-  pointY = pointY + height - angle * 2;
-  array[3] = [pointX, pointY];
-
-  pointX = pointX - angle;
-  pointY = pointY + angle;
-  array[4] = [pointX, pointY];
-
-  pointX = pointX - width + angle * 2;
-  array[5] = [pointX, pointY];
-
-  pointX = pointX - angle;
-  pointY = pointY - angle;
-  array[6] = [pointX, pointY];
-
-
-  pointY = pointY - height + angle * 2;
-  array[7] = [pointX, pointY];
-
-  return array;
-};
-
-var drawsMessage = function (ctx, startingPointСoordinates, width, height, angle, color) {
-  var array = getСloudСoordinates(startingPointСoordinates, width, height, angle);
-
-  ctx.beginPath();
-  ctx.moveTo(startingPointСoordinates[0], startingPointСoordinates[1] + angle);
-
-  for (var i = 0; i < array.length; i++) {
-    ctx.lineTo(array[i][0], array[i][1]);
-  }
-
-  ctx.closePath();
-  ctx.stroke();
-  ctx.fillStyle = color;
-  ctx.fill();
-};
-
 var getMaxTime = function (arr) {
   var maxTime = arr[0] || 0;
 
@@ -62,83 +11,157 @@ var getMaxTime = function (arr) {
   return maxTime;
 };
 
-var renderText = function (ctx, string, x, y, paragraph) {
-
-  while (string.indexOf('\n') + 1) {
-    var subString = string.slice(0, string.indexOf('\n'));
-
-    ctx.fillText(subString, x, y);
-
-    y += paragraph;
-
-    string = string.slice(string.indexOf('\n') + 1);
-  }
-
-  ctx.fillText(string, x, y);
-};
-
-var renderColumn = function (ctx, x, y, width, height, color, text, textIndent) {
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-
-  ctx.lineTo(x, y - height);
-  ctx.lineTo(x + width, y - height);
-  ctx.lineTo(x + width, y);
-  ctx.lineTo(x, y);
-  ctx.closePath();
-  ctx.fillStyle = color;
-  ctx.fill();
-
-  ctx.fillStyle = 'hsl(0, 0%, 0%)';
-  ctx.fillText(text, x, y + textIndent);
-};
-
 var getColor = function () {
-  var h = Math.round(200 + Math.random() * 25);
-  var s = Math.round(60 + Math.random() * 40);
-  var l = Math.round(25 + Math.random() * 25);
+  var h = Math.round(200 + Math.random() * 40);
+  var s = Math.round(40 + Math.random() * 60);
+  var l = Math.round(20 + Math.random() * 30);
+
   return 'hsl(' + h + ',' + s + '%,' + l + '%)';
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  var STARTING_POINT_COORDINATES = [100, 10];
-  var MESSAGE_WIDTH = 420;
-  var MESSAGE_HEIGHT = 270;
-  var MESSAGE_ANGLE = 20;
   var SHADOW_SHIFT = [110, 20];
-  var MESSAGE_TEXT = 'Ура вы победили!\nСписок результатов:';
   var MESSAGE_FONT = '16px PT Mono';
-  var LEFT_TEXT_INDENT = 170;
-  var TOP_TEXT_INDENT = 20;
-  var COLUMN_WIDTH = 40;
-  var COLUMN_HEIGHT = 150;
-  var COLUMN_INDENT = 50;
-  var HISTOGRAM_BASE_LINE = 230;
-  var LEFT_HISTOGRAM_INDENT = 150;
-  var USER_COLOR = 'rgba(255, 0, 0, 1)';
+  var cloud = {
+    color: 'hsl(0, 0%, 100%)',
+    start: [100, 10],
+    width: 420,
+    height: 270,
+    angle: 20,
+    drawsMessage: function (color, start, width, height, angle) {
+      color = color || this.color;
+      start = start || this.start;
+      width = width || this.width;
+      height = height || this.height;
+      angle = angle || this.angle;
+
+      var coordinates = this.getСloudСoordinates(start, width, height, angle);
+
+      ctx.beginPath();
+      ctx.moveTo(start[0], start[1] + angle);
+
+      for (var i = 0; i < coordinates.length; i++) {
+        ctx.lineTo(coordinates[i][0], coordinates[i][1]);
+      }
+
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.fillStyle = color;
+      ctx.fill();
+    },
+    getСloudСoordinates: function (start, width, height, angle) {
+      start = start || this.start;
+      width = width || this.width;
+      height = height || this.height;
+      angle = angle || this.angle;
+      var array = [];
+
+      var pointX = start[0] + angle;
+      var pointY = start[1];
+      array[0] = [pointX, pointY];
+
+      pointX = pointX + width - angle * 2;
+      array[1] = [pointX, pointY];
+
+      pointX = pointX + angle;
+      pointY = pointY + angle;
+      array[2] = [pointX, pointY];
+
+      pointY = pointY + height - angle * 2;
+      array[3] = [pointX, pointY];
+
+      pointX = pointX - angle;
+      pointY = pointY + angle;
+      array[4] = [pointX, pointY];
+
+      pointX = pointX - width + angle * 2;
+      array[5] = [pointX, pointY];
+
+      pointX = pointX - angle;
+      pointY = pointY - angle;
+      array[6] = [pointX, pointY];
+
+
+      pointY = pointY - height + angle * 2;
+      array[7] = [pointX, pointY];
+
+      return array;
+    }
+  };
+  var message = {
+    start: [170, 20],
+    text: 'Ура вы победили!\nСписок результатов:',
+    interval: 20,
+    renderText: function (start, text, interval) {
+      start = start || this.start;
+      text = text || this.text;
+      interval = interval || this.interval;
+
+      while (text.indexOf('\n') + 1) {
+        var subString = text.slice(0, text.indexOf('\n'));
+
+        ctx.fillText(subString, start[0], start[1]);
+
+        start[1] += interval;
+        text = text.slice(text.indexOf('\n') + 1);
+      }
+
+      ctx.fillText(text, start[0], start[1]);
+    }
+  };
+  var histogram = {
+    columnWidth: 40,
+    columnHeight: 150,
+    columnInterval: 50,
+    textIndent: 10,
+    start: [150, 230],
+    userColor: 'rgba(255, 0, 0, 1)',
+    renderColumn: function (text, start, color, height, width, textIndent) {
+      var x = start[0];
+      var y = start[1];
+      height = height || this.columnHeight;
+      width = width || this.columnWidth;
+      textIndent = textIndent || this.textIndent;
+
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x, y - height);
+      ctx.lineTo(x + width, y - height);
+      ctx.lineTo(x + width, y);
+      ctx.lineTo(x, y);
+      ctx.closePath();
+
+      ctx.fillStyle = color;
+      ctx.fill();
+      ctx.fillStyle = 'hsl(0, 0%, 0%)';
+      ctx.fillText(text, x, y + textIndent);
+    },
+    makeHistogram: function (start) {
+      var heightFactor = this.columnHeight / getMaxTime(times);
+      start = start || this.start;
+
+      for (var i = 0; i < names.length; i++) {
+        var height = Math.floor(heightFactor * times[i]);
+        var color = (names[i] === 'Вы') ? this.userColor : getColor();
+
+        this.renderColumn(names[i], start, color, height);
+
+        start[0] += this.columnWidth + this.columnInterval;
+      }
+    }
+  };
 
   // Если в массивах разное количество элементов, отбрасываем лишние у того, что длинее до идентичной со вторым массивом длины.
   names.length = times.length = (names.length > times.length) ? times.length : names.length;
 
-  drawsMessage(ctx, SHADOW_SHIFT, MESSAGE_WIDTH, MESSAGE_HEIGHT, MESSAGE_ANGLE, 'hsla(0, 0%, 0%, 0.7)');
-  drawsMessage(ctx, STARTING_POINT_COORDINATES, MESSAGE_WIDTH, MESSAGE_HEIGHT, MESSAGE_ANGLE, 'hsl(0, 0%, 100%)');
+  cloud.drawsMessage('hsla(0, 0%, 0%, 0.7)', SHADOW_SHIFT);
+  cloud.drawsMessage();
 
   ctx.font = MESSAGE_FONT;
   ctx.fillStyle = 'hsl(0, 0%, 0%)';
   ctx.textBaseline = 'hanging';
 
-  renderText(ctx, MESSAGE_TEXT, LEFT_TEXT_INDENT, SHADOW_SHIFT[1], TOP_TEXT_INDENT);
-
-  var heightFactor = COLUMN_HEIGHT / getMaxTime(times);
-  var x = LEFT_HISTOGRAM_INDENT;
-  var y = HISTOGRAM_BASE_LINE;
-
-  for (var i = 0; i < names.length; i++) {
-    var height = Math.floor(heightFactor * times[i]);
-    var color = (names[i] === 'Вы') ? USER_COLOR : getColor();
-
-    renderColumn(ctx, x, y, COLUMN_WIDTH, height, color, names[i], TOP_TEXT_INDENT);
-
-    x += COLUMN_WIDTH + COLUMN_INDENT;
-  }
+  message.renderText();
+  histogram.makeHistogram();
 };
