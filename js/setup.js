b3c3f26739b4db3560aa3b63c526c3af68f7099e
Odlopez <Odlopez@inbox.ru>
2018-06-06 10:51:10 +0300

    Группирует код и избавляется от магических чисел

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var settingsWindow = document.querySelector('.setup');
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвин'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_QUANTYTI = 4;
+var settingsWindow = document.querySelector('.setup');
 var similarCharacters = document.querySelector('.setup-similar');
 var similarListElement = similarCharacters.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
 
 var getRandomArrayElement = function (arr) {
   var randomIndexNumber = Math.floor(Math.random() * arr.length);
@@ -43,11 +45,11 @@ var renderWizard = function (wizard) {
 
 settingsWindow.classList.remove('hidden');
 
-var wizards = generateWizardsParameters(4);
+var wizards = generateWizardsParameters(WIZARDS_QUANTYTI);
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < WIZARDS_QUANTYTI; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 
