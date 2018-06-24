dbdac82dea8f0150c84bf419ae73cc4042b19329
Odlopez <Odlopez@inbox.ru>
2018-06-24 16:08:15 +0300

    Добавляет DOM-элемент с описанием ошибки

--- js/wizard.js
+++ js/wizard.js
@@ -30,7 +30,25 @@
   };
 
   var onError = function (message) {
-    document.body.textContent = message;
+    var popup = document.createElement('div');
+
+    var onDcumentClick = function () {
+      document.body.removeChild(popup);
+
+      document.removeEventListener('click', onDcumentClick);
+      document.removeEventListener('keydown', onDcumentKeydown);
+    };
+
+    var onDcumentKeydown = function (evt) {
+      window.utilities.isEscEvent(evt, onDcumentClick);
+    };
+
+    popup.style = 'position: fixed; display: flex; justify-content: center; align-items: center; background-color: rgba(0, 0, 0, 0.8); font-size: 50px; padding: 10px 50px; color: rgb(255, 255, 255); text-align: center; z-index: 10; width: 100%; height: 100%;';
+    popup.textContent = message;
+
+    document.body.insertAdjacentElement('afterbegin', popup);
+    document.addEventListener('click', onDcumentClick);
+    document.addEventListener('keydown', onDcumentKeydown);
   };
 
   window.backend.load(onLoad, onError);
